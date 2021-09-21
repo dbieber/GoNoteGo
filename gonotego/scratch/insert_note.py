806d08c023580e177ce0ae9aa515130d9ad34141
David Bieber <david810@gmail.com>
2021-09-20 22:12:55 -0400

    full path in insert

--- gonotego/scratch/insert_note.py
+++ gonotego/scratch/insert_note.py
@@ -5,7 +5,7 @@ from gonotego.common import interprocess
 
 def insert():
   note_events_queue = interprocess.get_note_events_queue()
-  note_event = events.NoteEvent('Test transcript', 'out/20210920-1632188898638.wav')
+  note_event = events.NoteEvent('Test transcript', '/home/pi/code/github/dbieber/GoNoteGo/out/20210920-1632188898638.wav')
   note_events_queue.put(bytes(note_event))
   print(note_events_queue.size())
   return 'Success'
