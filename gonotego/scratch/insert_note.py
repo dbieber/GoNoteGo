76736bb48b4724bc898fdbb840acb0b1ec90bbeb
David Bieber <david810@gmail.com>
2021-09-20 22:07:11 -0400

    Print size

--- gonotego/scratch/insert_note.py
+++ gonotego/scratch/insert_note.py
@@ -7,6 +7,8 @@ def insert():
   note_events_queue = interprocess.get_note_events_queue()
   note_event = events.NoteEvent('Test transcript', 'out/20210920-1632188898638.wav')
   note_events_queue.put(bytes(note_event))
+  print(note_events_queue.size())
+  return 'Success'
 
 
 if __name__ == '__main__':
