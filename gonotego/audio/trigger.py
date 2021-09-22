24c859e7493938895a4b0270d171f54bb88a6747
David Bieber <david810@gmail.com>
2021-09-22 14:13:35 -0400

    Make board optional in audio.trigger

--- gonotego/audio/trigger.py
+++ gonotego/audio/trigger.py
@@ -1,17 +1,27 @@
-import board
-from digitalio import DigitalInOut, Direction, Pull
+try:
+  import board
+  from digitalio import DigitalInOut, Direction, Pull
+except:
+  print('Unable to import board and digitalio.')
+  board = None
 import keyboard
 
 from gonotego.settings import secure_settings
 
 
-button = DigitalInOut(board.D17)
-button.direction = Direction.INPUT
-button.pull = Pull.UP
-
+if board is not None:
+  button = DigitalInOut(board.D17)
+  button.direction = Direction.INPUT
+  button.pull = Pull.UP
+else:
+  button = None
 
 def is_pressed():
+  if button is not None:
+    button_pressed = not button.value
+  else:
+    button_pressed = False
   return (
       keyboard.is_pressed(secure_settings.HOTKEY)
-      or not button.value
+      or button_pressed
   )
