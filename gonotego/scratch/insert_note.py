76b8ad2700bbe36b847f321944a997d57cc04a6a
David Bieber <david810@gmail.com>
2021-09-22 20:52:35 -0400

    get_status scratch command

--- gonotego/scratch/insert_note.py
+++ gonotego/scratch/insert_note.py
@@ -2,6 +2,8 @@ import fire
 
 from gonotego.common import events
 from gonotego.common import interprocess
+from gonotego.common import status
+
 
 def insert(text='test transcript', filepath=None):
   filepath = filepath or 'out/20210920-1632188898638.wav'
@@ -16,6 +18,11 @@ def size():
   note_events_queue = interprocess.get_note_events_queue()
   print(note_events_queue.size())
   return 'Success'
+
+
+def get_status():
+  for key in list(status.Status):
+    print(status.get(key))
 
 
 if __name__ == '__main__':
