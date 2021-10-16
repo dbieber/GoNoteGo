1c9e4d50d1420159167ed2fb3519665e12020e4d
David Bieber <david810@gmail.com>
2021-10-16 09:55:09 -0400

    Add tests for event serialization and deserialization

---
+++ gonotego/common/test_events.py
@@ -0,0 +1,36 @@
+import unittest
+from gonotego.common import events
+
+
+class ModelsTest(unittest.TestCase):
+
+  def test_audio_event(self):
+    event = events.AudioEvent(
+        action=events.AUDIO_DONE,
+        filepath='/tmp/audio')
+    event_bytes = bytes(event)
+    event2 = events.AudioEvent.from_bytes(event_bytes)
+    self.assertEqual(event, event2)
+
+  def test_command_event(self):
+    event = events.CommandEvent(
+        command_text='time')
+    event_bytes = bytes(event)
+    event2 = events.CommandEvent.from_bytes(event_bytes)
+    self.assertEqual(event, event2)
+
+  def test_note_event(self):
+    event = events.NoteEvent(
+        text='Example note.',
+        audio_filepath='/tmp/audio')
+    event_bytes = bytes(event)
+    event2 = events.NoteEvent.from_bytes(event_bytes)
+    self.assertEqual(event, event2)
+
+  def test_led_event(self):
+    event = events.LEDEvent(
+        color=[0, 0, 0],
+        ids=[0])
+    event_bytes = bytes(event)
+    event2 = events.LEDEvent.from_bytes(event_bytes)
+    self.assertEqual(event, event2)
