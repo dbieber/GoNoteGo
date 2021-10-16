deb3104b7cf93395df977504cdfe397dedec3907
David Bieber <david810@gmail.com>
2021-10-16 12:08:51 -0400

    mem import

--- gonotego/uploader/runner.py
+++ gonotego/uploader/runner.py
@@ -9,7 +9,7 @@ from gonotego.settings import secure_settings
 from gonotego.uploader.ideaflow import ideaflow_uploader
 from gonotego.uploader.remnote import remnote_uploader
 from gonotego.uploader.roam import roam_uploader
-from gonotego.uploader.roam import mem_uploader
+from gonotego.uploader.mem import mem_uploader
 
 Status = status.Status
 
