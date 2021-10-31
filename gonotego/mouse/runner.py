2450284278d519f3e084e6f5b82f35fcf75e79e1
David Bieber <david810@gmail.com>
2021-10-31 14:35:31 -0400

    Mouse prints

--- gonotego/mouse/runner.py
+++ gonotego/mouse/runner.py
@@ -1,4 +1,5 @@
 import mouse
+import sys
 import time
 
 from gonotego.common import status
@@ -7,14 +8,22 @@ Status = status.Status
 
 
 def handle_mouse_event(event):
+  print(event)
+  sys.stdout.flush()
   if isinstance(event, mouse.MoveEvent):
     status.set(Status.MOUSE_LAST_MOVE, time.time())
 
 
 def main():
+  print('prehook')
+  sys.stdout.flush()
   mouse.hook(handle_mouse_event)
+  print('posthook')
+  sys.stdout.flush()
   while True:
     time.sleep(5)
+    print('sleep')
+    sys.stdout.flush()
 
 if __name__ == '__main__':
   main()
