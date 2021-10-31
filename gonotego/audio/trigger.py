c8b4c900405a9fd21aceab07f2b54014ae94b1bb
David Bieber <david810@gmail.com>
2021-10-30 21:57:01 -0400

    Revert to working state

--- gonotego/audio/trigger.py
+++ gonotego/audio/trigger.py
@@ -13,12 +13,12 @@ if board is not None:
   # The "onboard button" is the physical button on the Voice Bonnet.
   onboard_button = DigitalInOut(board.D17)
   onboard_button.direction = Direction.INPUT
-  onboard_button.pull = Pull.DOWN
+  onboard_button.pull = Pull.UP
 
   # The "red button" is the handheld round one that's really satisfying to push.
   red_button = DigitalInOut(board.D27)
   red_button.direction = Direction.INPUT
-  red_button.pull = Pull.DOWN
+  red_button.pull = Pull.UP
 else:
   onboard_button = None
   red_button = None
@@ -27,8 +27,8 @@ else:
 def is_pressed():
   if onboard_button is not None:
     button_pressed = (
-        onboard_button.value
-        or red_button.value
+        not onboard_button.value
+        or not red_button.value
     )
   else:
     button_pressed = False
