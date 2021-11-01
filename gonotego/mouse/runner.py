a2a2e954e385d6dd1513e6dfab5a7fcf67501463
David Bieber <david810@gmail.com>
2021-11-01 18:30:38 -0400

    Remove flushes in mouse runner

--- gonotego/mouse/runner.py
+++ gonotego/mouse/runner.py
@@ -8,18 +8,14 @@ Status = status.Status
 
 
 def handle_mouse_event(event):
-  sys.stdout.flush()
   if isinstance(event, mouse.MoveEvent):
     status.set(Status.MOUSE_LAST_MOVE, time.time())
 
 
 def main():
-  sys.stdout.flush()
   mouse.hook(handle_mouse_event)
-  sys.stdout.flush()
   while True:
     time.sleep(5)
-    sys.stdout.flush()
 
 if __name__ == '__main__':
   main()
