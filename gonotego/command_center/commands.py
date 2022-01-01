7bad21a95b957212f7674abd3694fe83712a1a44
David Bieber <david810@gmail.com>
2022-01-01 18:31:40 -0500

    :ip command and json serialization Roam uploader fix (#19)
    * IP address command (:ip)
    * Use json instead of replace to fix special characters issue in Roam uploader

--- gonotego/command_center/commands.py
+++ gonotego/command_center/commands.py
@@ -121,6 +121,6 @@ def set_volume(value):
 
 
 @register_command('ip')
-def ip_address(value):
-  hostname_output = subprocess.check_output('hostname -I')
+def ip_address():
+  hostname_output = subprocess.check_output(['hostname', '-I']).decode('utf-8').strip()
   say(hostname_output)
