e7d421384d3f1d57ab0962e0b4847e48a9c55a72
ddohan <git@ddohan.com>
2022-01-01 22:33:57 +0000

    [remnote] Upload under specified root rem

--- gonotego/uploader/remnote/remnote_uploader.py
+++ gonotego/uploader/remnote/remnote_uploader.py
@@ -4,33 +4,39 @@ import requests
 from gonotego.settings import secure_settings
 from gonotego.uploader.blob import blob_uploader
 
-
 CREATE_URL = 'https://api.remnote.io/api/v0/create'
 
 
 def create_rem(text, edit_later, parent_id=None):
-  response = requests.post(
-      url=CREATE_URL,
-      data=dict(
+  data = dict(
           apiKey=secure_settings.REMNOTE_API_KEY,
           userId=secure_settings.REMNOTE_USER_ID,
           text=text,
           addToEditLater=edit_later,
           parentId=parent_id,
-          source='Go Note Go',
-      ),
+          isDocument=False,
   )
+  response = requests.post(
+      url=CREATE_URL,
+      json=data)
   return response.json().get('remId')
 
 
 class Uploader:
 
   def upload(self, note_events):
+    if not secure_settings.REMNOTE_ROOT_REM:
+        raise ValueError('Must provide REMNOTE_ROOT_REM')
+
     client = blob_uploader.make_client()
     for note_event in note_events:
       # Notes with audio should be checked for accuracy.
       edit_later = bool(note_event.audio_filepath)
-      rem_id = create_rem(note_event.text, edit_later)
+
+      rem_id = create_rem(
+              text=note_event.text,
+              edit_later=edit_later,
+              parent_id=secure_settings.REMNOTE_ROOT_REM)
       if note_event.audio_filepath and os.path.exists(note_event.audio_filepath):
         url = blob_uploader.upload_blob(note_event.audio_filepath, client)
         audio_rem_id = create_rem(url, edit_later=False, parent_id=rem_id)
