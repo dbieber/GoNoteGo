10822db711958b521efca5c1d1e51101074c55c6
David Bieber <david810@gmail.com>
2022-01-01 18:32:06 -0500

    Style commit for remnote uploader

--- gonotego/uploader/remnote/remnote_uploader.py
+++ gonotego/uploader/remnote/remnote_uploader.py
@@ -9,12 +9,12 @@ CREATE_URL = 'https://api.remnote.io/api/v0/create'
 
 def create_rem(text, edit_later, parent_id=None):
   data = dict(
-    apiKey=secure_settings.REMNOTE_API_KEY,
-    userId=secure_settings.REMNOTE_USER_ID,
-    text=text,
-    addToEditLater=edit_later,
-    parentId=parent_id,
-    isDocument=False,
+      apiKey=secure_settings.REMNOTE_API_KEY,
+      userId=secure_settings.REMNOTE_USER_ID,
+      text=text,
+      addToEditLater=edit_later,
+      parentId=parent_id,
+      isDocument=False,
   )
   response = requests.post(
       url=CREATE_URL,
@@ -34,9 +34,9 @@ class Uploader:
       edit_later = bool(note_event.audio_filepath)
 
       rem_id = create_rem(
-        text=note_event.text,
-        edit_later=edit_later,
-        parent_id=secure_settings.REMNOTE_ROOT_REM)
+          text=note_event.text,
+          edit_later=edit_later,
+          parent_id=secure_settings.REMNOTE_ROOT_REM)
       if note_event.audio_filepath and os.path.exists(note_event.audio_filepath):
         url = blob_uploader.upload_blob(note_event.audio_filepath, client)
         audio_rem_id = create_rem(url, edit_later=False, parent_id=rem_id)
