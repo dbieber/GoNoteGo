dfbd69c9024705b0ce16e3a30d9a629c72f85f4a
David Bieber <david810@gmail.com>
2022-01-22 12:50:52 -0500

    email

--- gonotego/command_center/email_commands.py
+++ gonotego/command_center/email_commands.py
@@ -1,8 +1,4 @@
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email import Encoders
-import getpass
+from email import message
 import smtplib
 import os
 
@@ -14,29 +10,26 @@ register_command = registry.register_command
 
 @register_command('email {} {}: {}')
 def _email(to, subject, text):
-   email(to, subject, text)
+  email(to, subject, text)
 
 
 def email(to, subject, text, attach=None):
-   email_user = settings.get('EMAIL_USER')
-   email_pwd = settings.get('EMAIL_PASSWORD')
-   email_server = settings.get('EMAIL_SERVER') or 'smtp.gmail.com'
+  email_user = settings.get('EMAIL_USER')
+  email_pwd = settings.get('EMAIL_PASSWORD')
+  email_server = settings.get('EMAIL_SERVER') or 'smtp.gmail.com'
 
-   msg = MIMEMultipart()
-   msg['From'] = email_user
-   msg['To'] = to
-   msg['Subject'] = subject
-   msg.attach(MIMEText(text))
-   if attach:
-      part = MIMEBase('application', 'octet-stream')
-      part.set_payload(open(attach, 'rb').read())
-      Encoders.encode_base64(part)
-      part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
-      msg.attach(part)
-   mailServer = smtplib.SMTP(email_server, 587)
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.ehlo()
-   mailServer.login(email_user, email_pwd)
-   mailServer.sendmail(email_user, to, msg.as_string())
-   mailServer.close()
+  msg = message.EmailMessage()
+  msg.set_content(text)
+
+  msg['Subject'] = f'The contents of {textfile}'
+  msg['From'] = email_user
+  msg['To'] = to
+  if email_server and email_pwd:
+    server = smtplib.SMTP(email_server, 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(email_user, email_pwd)  else:
+    server = smtplib.SMTP('localhost')
+  server.send_message(msg)
+  server.quit()
