f37f6c09c4ef1f7080003fc6216706e1e7415904
David Bieber <david810@gmail.com>
2023-03-26 23:01:06 -0400

    Toward support for custom commands

--- gonotego/command_center/commands.py
+++ gonotego/command_center/commands.py
@@ -6,6 +6,7 @@ It also defines some miscellaneous commands itself.
 from gonotego.common import events
 from gonotego.common import interprocess
 from gonotego.command_center import assistant_commands  # noqa: unused-import
+from gonotego.command_center import custom_commands  # noqa: unused-import
 from gonotego.command_center import note_commands  # noqa: unused-import
 from gonotego.command_center import settings_commands  # noqa: unused-import
 from gonotego.command_center import system_commands  # noqa: unused-import
