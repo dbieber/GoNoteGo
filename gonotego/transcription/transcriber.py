87b3dcde1860e1d7f960fabef29b5b9def15ee5f
David Bieber <david810@gmail.com>
2023-09-29 13:12:10 -0400

    Use openai whisper for transcription on Go Note Go

--- gonotego/transcription/transcriber.py
+++ gonotego/transcription/transcriber.py
@@ -1,36 +1,16 @@
 import io
 
 import fire
-
-from google.api_core import exceptions
-from google.cloud import speech
+import openai
 
 
 class Transcriber:
 
-  def __init__(self):
-    self.client = speech.SpeechClient()
-    self.config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-        sample_rate_hertz=44100,
-        language_code='en-US',
-    )
-
   def transcribe(self, filepath):
     with io.open(filepath, 'rb') as audio_file:
-      content = audio_file.read()
-
-    audio = speech.RecognitionAudio(content=content)
-    try:
-      response = self.client.recognize(config=self.config, audio=audio)
-    except exceptions.InvalidArgument:
-      # Could be: "payload size exceeds the limit: 10485760 bytes"
-      # TODO(dbieber): Support transcribing large clips.
-      return
-
-    return '\n'.join(
-        result.alternatives[0].transcript
-        for result in response.results)
+      response = openai.Audio.transcribe("whisper-1", file)
+      transcription = response['text']
+      return transcription.strip()
 
 
 if __name__ == '__main__':
